9771f9421987e24ad8c9c0cd3062a08d0a1ca5b2
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-03 02:29:20 +0530

    first commit

---
+++ src/watertankalarmsystem/pkg2/WaterTankAlarmSystem.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package watertankalarmsystem.pkg2;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Rensith Udara Gonalagoda
+ */
+public class WaterTankAlarmSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ControllRoom controllRoom = new ControllRoom();
+        controllRoom.addWaterLevelObserver(new Alarm());
+        controllRoom.addWaterLevelObserver(new Display());
+        controllRoom.addWaterLevelObserver(new SMSSender());
+        controllRoom.setVisible(true);
+    }
+
+}
